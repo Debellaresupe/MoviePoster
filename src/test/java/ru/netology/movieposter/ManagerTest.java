3fd8e134f03ba54dfd70a88d55590036039cc70f
Elnar Kilmukhametov <soulmiker@gmail.com>
2022-08-17 11:54:31 +0300

    add Mokito

--- src/test/java/ru/netology/movieposter/ManagerTest.java
+++ src/test/java/ru/netology/movieposter/ManagerTest.java
@@ -3,10 +3,13 @@ package ru.netology.movieposter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import static org.mockito.Mockito.*;
 
 public class ManagerTest {
-    MovieRepository repo = new MovieRepository();
+
+    MovieRepository repo = Mockito.mock(MovieRepository.class);
     Manager manager = new Manager(repo, 10);
 
     Movie film1 = new Movie(1, 11, "Film1", 100, 1);
@@ -16,17 +19,11 @@ public class ManagerTest {
     Movie film5 = new Movie(5, 55, "Film5", 500, 5);
     Movie film6 = new Movie(6, 66, "Film6", 600, 6);
 
-    @BeforeEach
-    public void setup() {
-        manager.save(film1);
-        manager.save(film2);
-        manager.save(film3);
-        manager.save(film4);
-        manager.save(film5);
-    }
-
     @Test
     public void shouldBeAddNewMovie() {
+
+        Movie[] movies = {film1, film2, film3, film4, film5};
+        doReturn(movies).when(repo).findAll();
 
         Movie[] expected = {
                 film1,
@@ -44,6 +41,15 @@ public class ManagerTest {
     @Test
     public void shouldBeReverseOrder() {
 
+        MovieRepository repo = new MovieRepository();
+        Manager manager = new Manager(repo, 10);
+
+        manager.save(film1);
+        manager.save(film2);
+        manager.save(film3);
+        manager.save(film4);
+        manager.save(film5);
+
         Movie[] expected = {
                 film5,
                 film4,
@@ -57,6 +63,15 @@ public class ManagerTest {
 
     @Test
     public void shouldBeRemoveAll() {
+
+        MovieRepository repo = new MovieRepository();
+        Manager manager = new Manager(repo, 10);
+
+        manager.save(film1);
+        manager.save(film2);
+        manager.save(film3);
+        manager.save(film4);
+        manager.save(film5);
 
         repo.removeAll();
 
